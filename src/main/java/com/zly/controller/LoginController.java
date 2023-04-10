f08d72d6089aa041553deb4a1bd37c37c6ae2004
zly <3065400350@qq.com>
2023-04-10 23:43:46 +0800

    modify some bug

--- src/main/java/com/zly/controller/LoginController.java
+++ src/main/java/com/zly/controller/LoginController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.util.Objects;
 
 @Controller
 public class LoginController {
@@ -29,10 +30,10 @@ public class LoginController {
     @RequestMapping("/login")
     public String login(HttpServletRequest httpServletRequest,HttpSession httpSession) throws InterruptedException {
         String username = httpServletRequest.getParameter("username");
+        System.out.println("username = " + username);
         String password = httpServletRequest.getParameter("password");
-        if (username==null||password==null){
-            return "login";
-        }else {
+        System.out.println("password = " + password);
+        if (!Objects.equals(username, "") && !Objects.equals(password, "")&&username!=null){
             User user = userService.findByUsername(username);
             if (user.getPassword().equals(password)){
                 //验证密码成功
@@ -42,16 +43,22 @@ public class LoginController {
             }else {
                 return "/";
             }
+        }else {
+            return "login";
         }
     }
     @PostMapping("/addUser")
     public String addUser(HttpServletRequest httpServletRequest) {
         String username = httpServletRequest.getParameter("username");
         String password = httpServletRequest.getParameter("password");
-        User user = new User();
-        user.setUsername(username);
-        user.setPassword(password);
-        userService.addUser(user);
-        return "redirect:/login";
+        if(username!=null&&password!=null){
+            User user = new User();
+            user.setUsername(username);
+            user.setPassword(password);
+            userService.addUser(user);
+            return "redirect:/login";
+        }else {
+            return "redirect:/login";
+        }
     }
 }
